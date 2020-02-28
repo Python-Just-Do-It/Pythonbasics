98d424ba506721f01e608cbaef1728d854db358a
NavatejaCherukuri <navateja@meratransport.com>
2020-02-28 15:51:51 +0530

    folders deleted

--- PythonDataTypes/PythonStringExamples.py
+++
@@ -1,9 +0,0 @@
-#split strings based on a charecter
-
-def splitString(inputString, splitChar = ' '):
-    return inputString.split(splitChar)
-
-print(splitString("abc,def,ghi", ','))
-print(splitString("qwe@rty@uio", '@'))
-print(splitString("abc def ghi"))
-
